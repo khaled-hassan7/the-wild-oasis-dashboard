9b77e60f1ba06d443e727a8a668e7dc5ca96e0d4
Khaled-Hassan7 <khaledhassan1907@gmail.com>
2026-01-22 13:59:08 +0200

    style: adjust headings, buttons, row , and images

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -47,3 +47,14 @@ const variations = {
     }
   `,
 };
+
+const Button = styled.button`
+  border: none;
+  border-radius: var(--border-radius-sm);
+  box-shadow: var(--shadow-sm);
+  cursor: pointer;
+  ${(props) => variations[props.variation]}
+  ${(props) => sizes[props.size]}
+`;
+
+export default Button;
