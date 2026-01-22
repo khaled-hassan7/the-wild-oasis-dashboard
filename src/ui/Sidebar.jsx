88f69e8647402bdce0ab5cd3ea00f9adc801ffd0
Khaled-Hassan7 <khaledhassan1907@gmail.com>
2026-01-22 13:47:15 +0200

    feat: add AppLayout with header and sidebar navigation

---
+++ src/ui/Sidebar.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import Logo from "./Logo";
+import MainNav from "./MainNav";
+const StyleSideBar = styled.aside`
+  background-color: var(--color-grey-0);
+  padding: 3.2rem 2.4rem;
+  border-right: 1px solid var(--color-grey-100);
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+
+  grid-row: 1/-1;
+`;
+function Sidebar() {
+  return (
+    <StyleSideBar>
+      <Logo />
+      <MainNav />
+    </StyleSideBar>
+  );
+}
+
+export default Sidebar;
