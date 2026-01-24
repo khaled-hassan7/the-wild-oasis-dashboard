62417b3ca4f5f473bc420f5ae257a61e72894a9c
Khaled-Hassan7 <khaledhassan1907@gmail.com>
2026-01-24 07:02:12 +0200

    feat: setup supabase integration

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://qzamhnfwkokabcliqtbi.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6YW1obmZ3a29rYWJjbGlxdGJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkwOTkwMDAsImV4cCI6MjA4NDY3NTAwMH0.iRqw9pm9YAGlaN4Puet8un0cbgTQKGVKbKBusuwBprI";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
