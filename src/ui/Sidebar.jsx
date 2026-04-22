f66f4195e085d12f5d38c6da32fdaca06b02e52d
Khaled-Hassan7 <khaledhassan1907@gmail.com>
2026-04-22 19:42:50 +0200

    feat(ui): add uploader component to the sidebar

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
+import Uploader from "../data/Uploader";
 const StyleSideBar = styled.aside`
   background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
@@ -16,6 +17,7 @@ function Sidebar() {
     <StyleSideBar>
       <Logo />
       <MainNav />
+      <Uploader />
     </StyleSideBar>
   );
 }
